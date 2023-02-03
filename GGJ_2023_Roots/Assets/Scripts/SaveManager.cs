2f58afd2884628d07c2554b93a5b2323a973fac5
Shane <Sbbergeron98@gmail.com>
2023-02-03 00:47:13 -0800

    small push 5

--- GGJ_2023_Roots/Assets/Scripts/SaveManager.cs
+++ GGJ_2023_Roots/Assets/Scripts/SaveManager.cs
@@ -1,11 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.InputSystem;
+using UnityEngine.InputSystem;
 
 public class SaveManager : MonoBehaviour
 {
-
     private void Start()
     {
         StartCoroutine(LoadGame());
@@ -18,13 +17,28 @@ public class SaveManager : MonoBehaviour
     }
     private void Update()
     {
-        //if (Keyboard.current.nKey.wasPressedThisFrame)
-       // {
+        if (Keyboard.current.nKey.wasPressedThisFrame)
+            Save();
+        if (Keyboard.current.nKey.wasPressedThisFrame)
+            Load();
 
-       // }
     }
     void Save()
     {
+        SaveData data = new SaveData();
+
+        // Camera position
+        //data.cameraPos = new SVec3(CameraController.instance.transform.position);
+        //data.cameraRot = new SVec3(CameraController.instance.transform.eulerAngles);
+        //data.cameraLook = new SVec3(CameraController.instance.cameraContainer.localEularAngles);
+
+        data.nutrients = GameManager.instance.nutrients;
+        data.food = GameManager.instance.curFood;
+        data.water = GameManager.instance.curWater;
+        data.curPopulation = GameManager.instance.curPopulation;
+        data.curJobs = GameManager.instance.curJobs;
+        data.maxJobs = GameManager.instance.maxJobs;
+        data.maxPopulation = GameManager.instance.maxPopulation;
 
     }
     void Load()
